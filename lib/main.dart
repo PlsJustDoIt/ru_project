a7d48ad0a36a3dfbcf3dec5b7952845c94b07896
Léo Maugeri <leomaugeri@hotmail.fr>
2024-08-26 11:06:08 +0200

    chore: Refactor main.dart to improve code readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,36 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: const Text('Projet ru de léo',),
+          title: const Text('Projet ru de léo'),
           backgroundColor: const Color.fromARGB(209, 66, 206, 62),
         ),
-        body:  Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('bienvenue à tous !',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.green,
-                ),
-              ),
-              const Text('(sauf à théo valfrey) ',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.green,
-                  fontFamily: 'Comic Sans MS',
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-              Image.asset("assets/images/jm.jpg",
-                width: 200,
-                height: 200,
-              ),
-            ],
-          ),
-        ),
+        body: const UserStateWidget(),
       ),
       title: 'Projet ru',
       theme: ThemeData(
@@ -51,109 +26,68 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class UserStateWidget extends StatefulWidget {
+  const UserStateWidget({super.key});
 
+  @override
+  _UserStateWidgetState createState() => _UserStateWidgetState();
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class _UserStateWidgetState extends State<UserStateWidget> {
+  String _currentState = "Inactif"; // État par défaut
 
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Projet ru',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 62, 183, 58)),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Projet ru de léo'),
-//     );
-//   }
-// }
+  void _changeState(String newState) {
+    setState(() {
+      _currentState = newState;
+    });
+  }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter = _counter + 4;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'État actuel : $_currentState',
+            style: const TextStyle(
+              fontSize: 24,
+              color: Colors.green,
+            ),
+          ),
+          const SizedBox(height: 20), // Espacement
+            Column(
+            children: [
+              Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => _changeState("Inactif"),
+                child: const Text('Inactif'),
+              ),
+              ),
+              Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => _changeState("Sur le point de manger"),
+                child: const Text('Sur le point de manger'),
+              ),
+              ),
+              Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => _changeState("A fini de manger"),
+                child: const Text('A fini de manger'),
+              ),
+              ),
+            ],
+            ),
+          Image.asset(
+            "assets/images/jm.jpg",
+            width: 200,
+            height: 200,
+          ),
+        ],
+      ),
+    );
+  }
+}
