b77d9665a24bcd72a69db7928b954b94394b849b
léo maugeri <leo.maugeri@edu.univ-fcomte.fr>
2024-09-03 11:38:47 +0200

    chore: Update MongoDB installation script for Ubuntu Jammy

--- install_backend.sh
+++ install_backend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-### Installation of mongodb
+
+source ~/.nvm/nvm.sh
+## Installation of mongodb
 
 sudo apt-get install gnupg curl -y
 
@@ -7,10 +9,8 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] \
-https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | \
-sudo tee -a /etc/apt/sources.list.d/mongodb-org-7.0.list
-
+UBUNTU_CODENAME=jammy
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_CODENAME/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl enable mongod.service # or sudo service mongod start
@@ -19,9 +19,12 @@ sudo systemctl enable mongod.service # or sudo service mongod start
 if [ ! -d "$HOME/.nvm" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
     source ~/.bashrc
+else 
+    echo "nvm already installed"
 fi
 
 ### Install nodejs
+nvm current
 nvm install --lts
 nvm use --lts
 
