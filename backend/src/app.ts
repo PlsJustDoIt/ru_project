e8d84fca6adde436dee6a65c36c7e9b5f84c00e3
léo maugeri <leo.maugeri@edu.univ-fcomte.fr>
2024-09-18 08:46:19 +0200

    Refactor server to support HTTPS in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,15 +4,21 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import ruRoutes from './routes/ru.js';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 
 import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 import { exit } from 'process';
 
 const app = express();
 console.log(process.env.MONGO_URI);
 
 mongoose.set("strictQuery", false);
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 if (process.env.MONGO_URI == null) {
   console.error('MONGO_URI is not defined');
@@ -32,4 +38,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/ru', ruRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (!isProduction) {
+  app.listen(PORT, () => console.log(`Server http running on port ${PORT}`));
+} else {
+  let options = {
+    cert: fs.readFileSync('/etc/ssl/private/server.key'),
+    key: fs.readFileSync('/etc/ssl/private/server.crt')
+  };
+let server = https.createServer(options,app);
+server.listen(PORT, () => console.log(`Server https running on port ${PORT}`));
+
+}
