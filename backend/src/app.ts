a7c03beb53c651d08662f071ccb02e5da003a4e0
Léo Maugeri <leomaugeri@hotmail.fr>
2024-11-30 21:47:34 +0100

    add of ginko api to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import ruRoutes from './routes/ru.js';
+import ginkoRoutes from './routes/ginko.js';
 import cors from 'cors';
 import fs from 'fs';
 import https from 'https';
@@ -13,54 +14,48 @@ import { exit } from 'process';
 import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
+    dotenv.config();
 }
-
 
 const app = express();
 logger.info('MONGO_URI: ' + process.env.MONGO_URI);
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-
 if (isProduction) {
-  console.log('lancement en production');
+    console.log('lancement en production');
 } else {
-  console.log('lancement en dev');
+    console.log('lancement en dev');
 }
 
-
-
 if (isProduction) {
-  //define dirnames
-  const __dirname = path.dirname(path.resolve());
-  //set up log file stream in logs folder
-  const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a+' });
-  //log requests combined format
-  app.use(morgan('combined', { stream: accessLogStream }));
+    // define dirnames
+    const __dirname = path.dirname(path.resolve());
+    // set up log file stream in logs folder
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a+' });
+    // log requests combined format
+    app.use(morgan('combined', { stream: accessLogStream }));
 } else {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
-
 
 if (process.env.MONGO_URI == null) {
-  logger.error('MONGO_URI is not defined');
-  exit(1);
+    logger.error('MONGO_URI is not defined');
+    exit(1);
 }
 
-
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => logger.info('MongoDB Connected'))
-  .catch(err => logger.error('MongoDB connection error:', err));
-
+    .then(() => logger.info('MongoDB Connected'))
+    .catch(err => logger.error('MongoDB connection error:', err));
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/ru', ruRoutes);
+app.use('/api/ginko', ginkoRoutes);
 
 const PORT = process.env.PORT || 5000;
 
